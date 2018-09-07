a0dc67a90251a620b582485815b0daac64e34c3e
YangS <sarayang.song@gmail.com>
2018-09-07 07:30:43 -0700

    adding first lintcode

---
+++ Pow_428.java
@@ -0,0 +1,22 @@
+package lintcode;
+
+/**
+ * Created by YANGSONG on 2018-09-05.
+ */
+public class Pow_428 {
+    /**
+     * @param x: the base number
+     * @param n: the power number
+     * @return: the result
+     */
+    public double myPow(double x, int n) {
+        // write your code here
+
+
+
+    }
+
+
+
+
+}
